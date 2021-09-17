d64445248388b15a72a77b795872b571e300cee1
David Rafe <david.rafe@gmail.com>
2021-09-17 14:03:42 -0400

    setup fetch to nasa api for 10 photos with useEffect in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import AstronomyContainer from './components/AstronomyContainer';
 
 function App() {
+
+  const [astronomyData, setAstronomyData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.nasa.gov/planetary/apod?count=10&api_key=sZRkR3ohLl9HFh3RqMCLquWm9tkhkWWd88yTNEv2')
+    .then(resp => resp.json())
+    .then(data => {
+      setAstronomyData(data)
+    })
+  }, [])
+
   return (
     <div className="App">
- 
+      <AstronomyContainer astronomyData={astronomyData}/>
     </div>
   );
 }
---
+++ src/components/AstronomyContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AstronomyContainer() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default AstronomyContainer
